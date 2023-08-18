1b8508b2570c4f1416730707def3f0c3eee178f7
Blain <dead3165@gmail.com>
2023-08-18 23:36:08 +0100

    rename and added api route

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 5000; //Line 3
+const path = require('path');
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// API routes and other Express configurations
+
+// Catchall route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
