63e4e5b522bdb90e4d4119bd935b2f9b1f344d65
Blain <dead3165@gmail.com>
2023-08-18 23:25:56 +0100

    fixed packages and gitignore

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+  res.send({ express: 'YOUR make BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
